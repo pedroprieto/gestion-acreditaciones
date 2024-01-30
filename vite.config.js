c907a1af5f6efb2991b4c164db2e77de068effc4
Pedro Prieto <pedpral@gmail.com>
2024-01-30 18:51:37 +0100

    Env

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: import.meta.env.BASE_URL || "",
   plugins: [
     vue({
       template: { transformAssetUrls }
