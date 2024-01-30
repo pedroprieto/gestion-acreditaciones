80beb64b971a8ff43ac509c64b7edba574f77fd9
Pedro Prieto <pedpral@gmail.com>
2024-01-30 11:38:13 +0100

    Navs para navegación

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -12,24 +12,38 @@ const drawer = ref(false);
 </script>
 
 <template>
-  <v-app-bar :elevation="2">
-    <v-app-bar-nav-icon
-      variant="text"
-      @click.stop="drawer = !drawer"
-    ></v-app-bar-nav-icon>
+  <!-- <v-app-bar :elevation="2"> -->
+  <!--   <v-app-bar-nav-icon -->
+  <!--     variant="text" -->
+  <!--     @click.stop="drawer = !drawer" -->
+  <!--   ></v-app-bar-nav-icon> -->
 
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
+  <!--   <v-app-bar-title> -->
+  <!--     <v-icon icon="mdi-file-document-multiple-outline" /> -->
+  <!--     Gestión del PEAC -->
+  <!--   </v-app-bar-title> -->
+  <!-- </v-app-bar> -->
 
-      Essentials Preset
-    </v-app-bar-title>
-  </v-app-bar>
+  <!-- <v-navigation-drawer v-model="drawer" temporary> -->
+  <!--   <v-list> -->
+  <!--     <v-list-item v-for="route of mainRoutes" :to="route.path">{{ -->
+  <!--       route.meta.prompt -->
+  <!--     }}</v-list-item> -->
+  <!--   </v-list> -->
+  <!-- </v-navigation-drawer> -->
+  <v-toolbar color="deep-purple-accent-4">
+    <v-toolbar-title>
+      <v-icon icon="mdi-file-document-multiple-outline" />
+      Gestión del PEAC
+    </v-toolbar-title>
+  </v-toolbar>
 
-  <v-navigation-drawer v-model="drawer" temporary>
-    <v-list>
-      <v-list-item v-for="route of mainRoutes" :to="route.path">{{
-        route.meta.prompt
-      }}</v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+  <v-tabs color="deep-purple-accent-4" align-tabs="center">
+    <v-tab
+      v-for="route of mainRoutes"
+      :to="route.path"
+      :value="route.meta.prompt"
+      >{{ route.meta.prompt }}</v-tab
+    >
+  </v-tabs>
 </template>
